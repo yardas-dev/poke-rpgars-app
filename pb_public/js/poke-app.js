2a4908c78463e7a21bb6fcfe41c1b199dea57128
yardas-dev <selector.yardas_0u@icloud.com>
2026-05-21 17:54:06 +0200

    Corregir formato con Prettier

--- pb_public/js/poke-app.js
+++ pb_public/js/poke-app.js
@@ -1,29 +1,26 @@
-import { html } from "https://cdn.jsdelivr.net/npm/lit@3.3.3/+esm"
+import { html } from "https://cdn.jsdelivr.net/npm/lit@3.3.3/+esm";
 
-import rutas from "./rutas.js"
-import { colecciones } from "./helpers/colecciones.js"
+import rutas from "./rutas.js";
+import { colecciones } from "./helpers/colecciones.js";
 
-import { ComponenteBase } from "./components/componente-base.js"
-import "./components/barra-navegacion.js"
+import { ComponenteBase } from "./components/componente-base.js";
+import "./components/barra-navegacion.js";
 
 class PokeApp extends ComponenteBase {
-    connectedCallback() {
-        super.connectedCallback()
-        colecciones.descargar()
-    }
+  connectedCallback() {
+    super.connectedCallback();
+    colecciones.descargar();
+  }
 
-    render() {
-        const rutaActual = rutas.find(
-            ruta => ruta.nombre === window.location.pathname
-        )
+  render() {
+    const rutaActual = rutas.find(
+      (ruta) => ruta.nombre === window.location.pathname,
+    );
 
-        return html`
-            <barra-navegacion></barra-navegacion>
-
-            <main class="section mb-6">
-                ${rutaActual.vista}
-            </main>
-        `
-    }
+    return html`
+      <barra-navegacion></barra-navegacion>
+      <main class="section mb-6">${rutaActual.vista}</main>
+    `;
+  }
 }
-customElements.define("poke-app", PokeApp)
+customElements.define("poke-app", PokeApp);
--- pb_public/js/rutas.js
+++ pb_public/js/rutas.js
@@ -1,49 +1,49 @@
-import { html } from "https://cdn.jsdelivr.net/npm/lit@3.3.3/+esm"
+import { html } from "https://cdn.jsdelivr.net/npm/lit@3.3.3/+esm";
 
-import "./views/vista-personajes.js"
-import "./views/vista-equipos.js"
-import "./views/vista-combates.js"
-import "./views/vista-pokedex.js"
-import "./views/vista-referencia.js"
-import "./views/vista-utilidades.js"
+import "./views/vista-personajes.js";
+import "./views/vista-equipos.js";
+import "./views/vista-combates.js";
+import "./views/vista-pokedex.js";
+import "./views/vista-referencia.js";
+import "./views/vista-utilidades.js";
 
 const rutas = [
-    {
-        nombre: "/",
-        titulo: "Personajes",
-        vista: html`<vista-personajes></vista-personajes>`,
-        esVisible: true,
-    },
-    {
-        nombre: "/equipos",
-        titulo: "Equipos",
-        vista: html`<vista-equipos></vista-equipos>`,
-        esVisible: true,
-    },
-    {
-        nombre: "/combates",
-        titulo: "Combates",
-        vista: html`<vista-combates></vista-combates>`,
-        esVisible: true,
-    },
-    {
-        nombre: "/pokedex",
-        titulo: "Pokédex",
-        vista: html`<vista-pokedex></vista-pokedex>`,
-        esVisible: true,
-    },
-    {
-        nombre: "/referencia",
-        titulo: "Referencia",
-        vista: html`<vista-referencia></vista-referencia>`,
-        esVisible: true,
-    },
-    {
-        nombre: "/utilidades",
-        titulo: "Utilidades",
-        vista: html`<vista-utilidades></vista-utilidades>`,
-        esVisible: false,
-    },
-]
+  {
+    nombre: "/",
+    titulo: "Personajes",
+    vista: html`<vista-personajes></vista-personajes>`,
+    esVisible: true,
+  },
+  {
+    nombre: "/equipos",
+    titulo: "Equipos",
+    vista: html`<vista-equipos></vista-equipos>`,
+    esVisible: true,
+  },
+  {
+    nombre: "/combates",
+    titulo: "Combates",
+    vista: html`<vista-combates></vista-combates>`,
+    esVisible: true,
+  },
+  {
+    nombre: "/pokedex",
+    titulo: "Pokédex",
+    vista: html`<vista-pokedex></vista-pokedex>`,
+    esVisible: true,
+  },
+  {
+    nombre: "/referencia",
+    titulo: "Referencia",
+    vista: html`<vista-referencia></vista-referencia>`,
+    esVisible: true,
+  },
+  {
+    nombre: "/utilidades",
+    titulo: "Utilidades",
+    vista: html`<vista-utilidades></vista-utilidades>`,
+    esVisible: false,
+  },
+];
 
-export default rutas
+export default rutas;
