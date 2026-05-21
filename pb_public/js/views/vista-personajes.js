f87c9a83e197ecfba8ed4877c0ab733660cc23f5
yardas-dev <selector.yardas_0u@icloud.com>
2026-05-21 17:57:45 +0200

    Corregir formato con Prettier en vista personajes

--- pb_public/js/views/vista-personajes.js
+++ pb_public/js/views/vista-personajes.js
@@ -1,31 +1,25 @@
-import { html } from "https://cdn.jsdelivr.net/npm/lit@3.3.3/+esm"
-import { colecciones } from "../helpers/colecciones.js"
-import { ComponenteBase } from "../components/componente-base.js"
-import "../components/ficha-personaje.js"
+import { html } from "https://cdn.jsdelivr.net/npm/lit@3.3.3/+esm";
+import { colecciones } from "../helpers/colecciones.js";
+import { ComponenteBase } from "../components/componente-base.js";
+import "../components/ficha-personaje.js";
 
 class VistaPersonajes extends ComponenteBase {
-    render() {
-        return html`
-            <p class="title">
-                Personajes
-            </p>
+  render() {
+    return html`
+      <p class="title">Personajes</p>
 
-            <div class="columns">
-                ${
-                    this.personajes.filter(
-                            personaje => personaje.rol === "jugador"
-                        ).map(
-                            personaje => html`
-                                <div class="column">
-                                    <ficha-personaje
-                                        .personaje=${personaje}
-                                    ></ficha-personaje>
-                                </div>
-                            `
-                        )
-                }
-            </div>
-        `
-    }
+      <div class="columns">
+        ${this.personajes
+          .filter((personaje) => personaje.rol === "jugador")
+          .map(
+            (personaje) => html`
+              <div class="column">
+                <ficha-personaje .personaje=${personaje}></ficha-personaje>
+              </div>
+            `,
+          )}
+      </div>
+    `;
+  }
 }
-customElements.define("vista-personajes", VistaPersonajes)
+customElements.define("vista-personajes", VistaPersonajes);
