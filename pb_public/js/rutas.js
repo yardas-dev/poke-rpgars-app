37c19027cb5329f4dacb4b256b8a8a50a92382de
yardas-dev <selector.yardas_0u@icloud.com>
2026-05-21 17:43:59 +0200

    Corregir ruta de encuentros antigua y añadir referencia y utilidades

--- pb_public/js/rutas.js
+++ pb_public/js/rutas.js
@@ -3,8 +3,9 @@ import { html } from "https://cdn.jsdelivr.net/npm/lit@3.3.3/+esm"
 import "./views/vista-personajes.js"
 import "./views/vista-equipos.js"
 import "./views/vista-combates.js"
-import "./views/vista-encuentros.js"
 import "./views/vista-pokedex.js"
+import "./views/vista-referencia.js"
+import "./views/vista-utilidades.js"
 
 const rutas = [
     {
@@ -31,12 +32,17 @@ const rutas = [
         vista: html`<vista-pokedex></vista-pokedex>`,
         esVisible: true,
     },
-        nombre: "/encuentros",
-        vista: html`<vista-encuentros></vista-encuentros>`
+    {
+        nombre: "/referencia",
+        titulo: "Referencia",
+        vista: html`<vista-referencia></vista-referencia>`,
+        esVisible: true,
     },
     {
-        nombre: "/pokedex",
-        vista: html`<vista-pokedex></vista-pokedex>`
+        nombre: "/utilidades",
+        titulo: "Utilidades",
+        vista: html`<vista-utilidades></vista-utilidades>`,
+        esVisible: false,
     },
 ]
 
