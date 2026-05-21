a91aa0213b9213ec9c2082bfdc8e5ead67936d40
yardas-dev <selector.yardas_0u@icloud.com>
2026-05-21 17:41:05 +0200

    Eliminar vista de encuentros

--- pb_public/js/views/vista-encuentros.js
+++
@@ -1,9 +0,0 @@
-import { html } from "https://cdn.jsdelivr.net/npm/lit@3.3.3/+esm"
-import { ComponenteBase } from "../components/componente-base.js"
-
-class VistaEncuentros extends ComponenteBase {
-    render() {
-        return html`<p class="title">Encuentros</p>`
-    }
-}
-customElements.define("vista-encuentros", VistaEncuentros)
